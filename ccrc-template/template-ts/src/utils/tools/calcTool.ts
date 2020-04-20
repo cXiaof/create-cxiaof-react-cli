414a2f58ec645520735cea3d6304472a5053cfc1
cXiaof <310257509@qq.com>
2020-04-20 14:28:57 +0800

    使用number-precision且calcTool不再提供自定义加减乘除

--- ccrc-template/template-ts/src/utils/tools/calcTool.ts
+++ ccrc-template/template-ts/src/utils/tools/calcTool.ts
@@ -1,17 +1,18 @@
+import NP from 'number-precision'
+
+let global: any = window
+
+global.GoldenSection = NP.round(NP.divide(NP.minus(Math.sqrt(5), 1), 2), 3)
+
 const calcTool = {
     calcMinUint: (num1: number = 1, num2: number = 1): number =>
         Math.max(0, Math.min(num1, num2)),
 
     calcGolden: (num: number, reverse: boolean): number => {
-        const golden = calcTool.floatCHU(
-            calcTool.floatJIAN(Math.sqrt(5), 1),
-            2,
-            3
-        )
-        return calcTool.floatCHEN(
-            num,
-            reverse ? calcTool.floatJIAN(1, golden) : golden
-        )
+        const divisor = reverse
+            ? NP.minus(1, global.GoldenSection)
+            : global.GoldenSection
+        return NP.times(num, divisor)
     },
 
     getTextWidth: (txt: string): number => {
@@ -24,81 +25,6 @@ const calcTool = {
         }
         return result
     },
-
-    floatJIA: (
-        num1: number = 0,
-        num2: number = 0,
-        fixed: number = 20
-    ): number => {
-        const [m] = getJiaJianVal(num1, num2)
-        const result = Math.round(num1 * m + num2 * m) / m
-        return parseFloat(result.toFixed(fixed))
-    },
-
-    floatJIAN: (
-        num1: number = 0,
-        num2: number = 0,
-        fixed: number = 20
-    ): number => {
-        const [m, r1, r2] = getJiaJianVal(num1, num2)
-        const result = (Math.round(num1 * m - num2 * m) / m).toFixed(
-            Math.max(r1 - 0, r2 - 0)
-        )
-        return parseFloat(parseFloat(result).toFixed(fixed))
-    },
-
-    floatCHEN: (
-        num1: number = 0,
-        num2: number = 1,
-        fixed: number = 20
-    ): number => {
-        let m = 0,
-            s1 = num1.toString(),
-            s2 = num2.toString()
-        try {
-            m += s1.split('.')[1].length
-        } catch (e) {}
-        try {
-            m += s2.split('.')[1].length
-        } catch (e) {}
-        const result =
-            (Number(s1.replace('.', '')) * Number(s2.replace('.', ''))) /
-            Math.pow(10, m)
-        return parseFloat(result.toFixed(fixed))
-    },
-
-    floatCHU: (
-        num1: number = 0,
-        num2: number = 1,
-        fixed: number = 20
-    ): number => {
-        const [t1, t2] = getDecimalLength(num1, num2)
-        const r1 = Number(num1.toString().replace('.', ''))
-        const r2 = Number(num2.toString().replace('.', ''))
-        const result = (r1 / r2) * Math.pow(10, t2 - t1)
-        return parseFloat(result.toFixed(fixed))
-    },
-}
-
-const getJiaJianVal = (num1: number, num2: number): number[] => {
-    const [r1, r2] = getDecimalLength(num1, num2)
-    const m = Math.pow(10, Math.max(r1, r2))
-    return [m, r1, r2]
-}
-
-const getDecimalLength = (num1: number, num2: number): number[] => {
-    let r1, r2
-    try {
-        r1 = num1.toString().split('.')[1].length
-    } catch (e) {
-        r1 = 0
-    }
-    try {
-        r2 = num2.toString().split('.')[1].length
-    } catch (e) {
-        r2 = 0
-    }
-    return [r1, r2]
 }
 
 export default calcTool
--- ccrc-template/template/src/utils/tools/calcTool.js
+++ ccrc-template/template/src/utils/tools/calcTool.js
@@ -1,16 +1,15 @@
+import NP from 'number-precision'
+
+global.GoldenSection = NP.round(NP.divide(NP.minus(Math.sqrt(5), 1), 2), 3)
+
 const calcTool = {
     calcMinUint: (num1 = 1, num2 = 1) => Math.max(0, Math.min(num1, num2)),
 
     calcGolden: (num, reverse) => {
-        const golden = calcTool.floatCHU(
-            calcTool.floatJIAN(Math.sqrt(5), 1),
-            2,
-            3
-        )
-        return calcTool.floatCHEN(
-            num,
-            reverse ? calcTool.floatJIAN(1, golden) : golden
-        )
+        const divisor = reverse
+            ? NP.minus(1, global.GoldenSection)
+            : global.GoldenSection
+        return NP.times(num, divisor)
     },
 
     getTextWidth: (txt) => {
@@ -23,65 +22,6 @@ const calcTool = {
         }
         return result
     },
-
-    floatJIA: (num1 = 0, num2 = 0, fixed = 20) => {
-        const [m] = getJiaJianVal(num1, num2)
-        const result = Math.round(num1 * m + num2 * m) / m
-        return parseFloat(result.toFixed(fixed))
-    },
-
-    floatJIAN: (num1 = 0, num2 = 0, fixed = 20) => {
-        const [m, r1, r2] = getJiaJianVal(num1, num2)
-        const result = (Math.round(num1 * m - num2 * m) / m).toFixed(
-            Math.max(r1 - 0, r2 - 0)
-        )
-        return parseFloat(parseFloat(result).toFixed(fixed))
-    },
-
-    floatCHEN: (num1 = 0, num2 = 1, fixed = 20) => {
-        let m = 0,
-            s1 = num1.toString(),
-            s2 = num2.toString()
-        try {
-            m += s1.split('.')[1].length
-        } catch (e) {}
-        try {
-            m += s2.split('.')[1].length
-        } catch (e) {}
-        const result =
-            (Number(s1.replace('.', '')) * Number(s2.replace('.', ''))) /
-            Math.pow(10, m)
-        return parseFloat(result.toFixed(fixed))
-    },
-
-    floatCHU: (num1 = 0, num2 = 1, fixed = 20) => {
-        const [t1, t2] = getDecimalLength(num1, num2)
-        const r1 = Number(num1.toString().replace('.', ''))
-        const r2 = Number(num2.toString().replace('.', ''))
-        const result = (r1 / r2) * Math.pow(10, t2 - t1)
-        return parseFloat(result.toFixed(fixed))
-    },
-}
-
-const getJiaJianVal = (num1, num2) => {
-    const [r1, r2] = getDecimalLength(num1, num2)
-    const m = Math.pow(10, Math.max(r1, r2))
-    return [m, r1, r2]
-}
-
-const getDecimalLength = (num1, num2) => {
-    let r1, r2
-    try {
-        r1 = num1.toString().split('.')[1].length
-    } catch (e) {
-        r1 = 0
-    }
-    try {
-        r2 = num2.toString().split('.')[1].length
-    } catch (e) {
-        r2 = 0
-    }
-    return [r1, r2]
 }
 
 export default calcTool
