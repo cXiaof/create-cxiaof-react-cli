c0bc961b71e630eac4af76b355a4c2a791ac8ee7
cXiaof <310257509@qq.com>
2020-04-22 15:09:09 +0800

    itemsTool新加getArrUpset方法

--- ccrc-template/template-ts/src/utils/tools/itemsTool.ts
+++ ccrc-template/template-ts/src/utils/tools/itemsTool.ts
@@ -1,6 +1,8 @@
 const itemsTool = {
     getArrNoRepeat: (arr: any[]): any[] => Array.from(new Set(arr)),
 
+    getArrUpset: (arr: any[]): any[] => arr.sort(() => 0.5 - Math.random()),
+
     packAttrToArr: (item: any | any[], callback?: any): any[] => {
         const arr = Array.isArray(item) ? item : [item]
         if (callback) arr.forEach(callback)
--- ccrc-template/template/src/utils/tools/itemsTool.js
+++ ccrc-template/template/src/utils/tools/itemsTool.js
@@ -1,6 +1,8 @@
 const itemsTool = {
     getArrNoRepeat: (arr) => Array.from(new Set(arr)),
 
+    getArrUpset: (arr) => arr.sort(() => 0.5 - Math.random()),
+
     packAttrToArr: (item, callback) => {
         const arr = Array.isArray(item) ? item : [item]
         if (callback) arr.forEach(callback)
