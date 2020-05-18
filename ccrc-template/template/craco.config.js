0f3ed43bdd571ced499a552128bdd43926508434
cXiaof <310257509@qq.com>
2020-05-18 11:31:51 +0800

    customize-cra -> craco

---
+++ ccrc-template/template/craco.config.js
@@ -0,0 +1,25 @@
+const CracoAlias = require('craco-alias')
+const px2rem = require('postcss-plugin-px2rem')
+
+const px2remOpts = {
+    rootValue: 16,
+    // unitPrecision: 5,
+    // propWhiteList: [],
+    // propBlackList: [],
+    // exclude: false,
+    // selectorBlackList: [],
+    // ignoreIdentifier: false,
+    // replace: true,
+    // mediaQuery: false,
+    minPixelValue: 3,
+}
+
+module.exports = {
+    plugins: [
+        {
+            plugin: CracoAlias,
+            options: { source: 'jsconfig', baseUrl: './src' },
+        },
+    ],
+    style: { postcss: { mode: 'extends', plugins: [px2rem(px2remOpts)] } },
+}
