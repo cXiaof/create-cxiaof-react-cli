c5f552b1c1a6ee4d4d4f2be0c904bdd9dd8d1418
cXiaof <310257509@qq.com>
2020-11-20 10:50:50 +0800

    使用更纯粹的请求

---
+++ ccrc-template/template/src/utils/Request.js
@@ -0,0 +1,32 @@
+import umiRequest, { extend } from 'umi-request'
+
+const request = extend({
+    // prefix: '/cdcs',
+})
+
+// request.interceptors.request.use((url, options) => {
+//     if (options.data && options.requestType === 'form')
+//         options.data = new URLSearchParams(
+//             Object.entries(options.data).map(([key, value]) => [
+//                 key,
+//                 typeof value === 'object' ? JSON.stringify(value) : value,
+//             ])
+//         )
+//     return { url, options }
+// })
+
+// request.interceptors.request.use((url, options) => {
+//     if (!options.noToken) {
+//         const Tingtoken = localStorage.getItem('Tingtoken')
+//         if (Tingtoken) options.headers = { Tingtoken }
+//     }
+//     return { url, options }
+// })
+
+export const getCancelToken = () => {
+    const CancelToken = umiRequest.CancelToken
+    const { token, cancel } = CancelToken.source()
+    return [token, cancel]
+}
+
+export default request
