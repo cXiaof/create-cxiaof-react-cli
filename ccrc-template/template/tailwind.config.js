32191093e8832efa6f43b412cc255903f931ee2c
cXiaof <310257509@qq.com>
2022-04-14 22:00:41 +0800

    tailwind+html

--- ccrc-template/template/tailwind.config.js
+++ ccrc-template/template/tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   corePlugins: {
     preflight: false,
   },
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
