3998c87c8eee4cc6a5bc25a7b4ad86909d8dacd4
cXiaof <310257509@qq.com>
2023-01-05 14:14:10 +0800

    重新组织utils代码结构

---
+++ ccrc-template/template-ts/src/utils/index.ts
@@ -0,0 +1,11 @@
+import * as asyncUtils from './async'
+import * as calcUtils from './calc'
+import * as itemsUtils from './items'
+
+const utils = {
+  async: asyncUtils,
+  calc: calcUtils,
+  items: itemsUtils,
+}
+
+export default utils
