1c7655cb1358e2e0236d4a1831d326e75dba193f
cXiaof <310257509@qq.com>
2023-02-07 16:55:59 +0800

    fmt tailwind config content

--- ccrc-template/template/tailwind.config.js
+++ ccrc-template/template/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['public/index.html', 'src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./public/index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
